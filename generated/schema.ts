da6534213540c2751ef9eb4dfc5cf9e58893b46a
james-gannon <mailforjamesg@gmail.com>
2023-01-25 10:18:59 -0500

    nft-marketplace.ts event listeners updated for the Graph deployment

--- generated/schema.ts
+++ generated/schema.ts
@@ -11,6 +11,83 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
+export class ActiveItem extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save ActiveItem entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type ActiveItem must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("ActiveItem", id.toString(), this);
+    }
+  }
+
+  static load(id: string): ActiveItem | null {
+    return changetype<ActiveItem | null>(store.get("ActiveItem", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get buyer(): Bytes {
+    let value = this.get("buyer");
+    return value!.toBytes();
+  }
+
+  set buyer(value: Bytes) {
+    this.set("buyer", Value.fromBytes(value));
+  }
+
+  get seller(): Bytes {
+    let value = this.get("seller");
+    return value!.toBytes();
+  }
+
+  set seller(value: Bytes) {
+    this.set("seller", Value.fromBytes(value));
+  }
+
+  get nftAddress(): Bytes {
+    let value = this.get("nftAddress");
+    return value!.toBytes();
+  }
+
+  set nftAddress(value: Bytes) {
+    this.set("nftAddress", Value.fromBytes(value));
+  }
+
+  get tokenId(): BigInt {
+    let value = this.get("tokenId");
+    return value!.toBigInt();
+  }
+
+  set tokenId(value: BigInt) {
+    this.set("tokenId", Value.fromBigInt(value));
+  }
+
+  get price(): BigInt {
+    let value = this.get("price");
+    return value!.toBigInt();
+  }
+
+  set price(value: BigInt) {
+    this.set("price", Value.fromBigInt(value));
+  }
+}
+
 export class ItemBought extends Entity {
   constructor(id: Bytes) {
     super();
--- src/nft-marketplace.ts
+++ src/nft-marketplace.ts
@@ -1,53 +1,135 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import {
   ItemBought as ItemBoughtEvent,
   ItemCanceled as ItemCanceledEvent,
-  ItemListed as ItemListedEvent
-} from "../generated/NftMarketplace/NftMarketplace"
-import { ItemBought, ItemCanceled, ItemListed } from "../generated/schema"
+  ItemListed as ItemListedEvent,
+} from "../generated/NftMarketplace/NftMarketplace";
+import {
+  ItemBought,
+  ItemCanceled,
+  ItemListed,
+  ActiveItem,
+} from "../generated/schema";
 
 export function handleItemBought(event: ItemBoughtEvent): void {
-  let entity = new ItemBought(
+  let itemBought = ItemBought.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  let activeItem = ActiveItem.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  if (!itemBought) {
+    itemBought = new ItemBought(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+    );
+  }
+  itemBought.buyer = event.params.buyer;
+  itemBought.nftAddress = event.params.nftAddress;
+  itemBought.tokenId = event.params.tokenId;
+  activeItem!.buyer = event.params.buyer;
+
+  itemBought.save();
+  activeItem!.save();
+
+  /*let entity = new ItemBought(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.buyer = event.params.buyer
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-  entity.price = event.params.price
+  );
+  entity.buyer = event.params.buyer;
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
+  entity.price = event.params.price;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();*/
 }
 
 export function handleItemCanceled(event: ItemCanceledEvent): void {
-  let entity = new ItemCanceled(
+  let itemCanceled = ItemCanceled.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  let activeItem = ActiveItem.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  if (!itemCanceled) {
+    itemCanceled = new ItemCanceled(
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+    );
+  }
+
+  itemCanceled.seller = event.params.seller;
+  itemCanceled.nftAddress = event.params.nftAddress;
+  itemCanceled.tokenId = event.params.tokenId;
+  activeItem!.buyer = Address.fromString(
+    "0x000000000000000000000000000000000000dEaD" // How we decide if an item has been bought or not
+  );
+
+  itemCanceled.save();
+  activeItem!.save();
+
+  /*let entity = new ItemCanceled(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
+  );
+  entity.seller = event.params.seller;
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();*/
 }
 
 export function handleItemListed(event: ItemListedEvent): void {
-  let entity = new ItemListed(
+  let itemListed = ItemListed.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  let activeItem = ActiveItem.load(
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
+  );
+  if (!itemListed) {
+    itemListed = new ItemListed(event.params.tokenId, event.params.nftAddress);
+  }
+  if (!activeItem) {
+    activeItem = new ActiveItem(event.params.tokenId, event.params.nftAddress);
+  }
+  itemListed.seller = event.params.seller;
+  activeItem.seller = event.params.seller;
+
+  itemListed.nftAddress = event.params.nftAddress;
+  activeItem.nftAddress = event.params.nftAddress;
+
+  itemListed.tokenId = event.params.tokenId;
+  activeItem.tokenId = event.params.tokenId;
+
+  itemListed.price = event.params.price;
+  activeItem.price = event.params.price;
+
+  activeItem.buyer = Address.fromString(
+    "0x0000000000000000000000000000000000000000"
+  );
+
+  itemListed.save();
+  activeItem.save();
+
+  /*let entity = new ItemListed(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-  entity.price = event.params.price
+  );
+  entity.seller = event.params.seller;
+  entity.nftAddress = event.params.nftAddress;
+  entity.tokenId = event.params.tokenId;
+  entity.price = event.params.price;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();*/
+}
+// Define the types of different parameters in TS
+function getIdFromEventParams(tokenId: BigInt, nftAddress: Address): string {
+  return tokenId.toHexString() + nftAddress.toHexString();
 }
